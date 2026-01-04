0c30c5f6b817b764b57bb7b78a9c6fa23f363ef8
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-01-04 11:08:04 +0800

    chore(app#build-info-str): use computed instead of func

--- src/App.vue
+++ src/App.vue
@@ -41,14 +41,14 @@ const switchTheme = () => {
 }
 
 const isPreview = JSON.parse(import.meta.env.VITE_IS_PREVIEW) as boolean;
-const genBuildInfoStr = () => {
+const buildInfoStr = computed(() => {
 	const deployedSHA = import.meta.env.VERCEL_GIT_COMMIT_SHA?.trim()?.slice(0, 7);
 	const isVercel = import.meta.env.VERCEL === '1';
 	let str = `Build at ${new Date(import.meta.env.VITE_BUILD_TIMESTAMP).toISOString()} `;
 	deployedSHA && (str += `with commit <a href="https://github.com/LateDreamXD/mcisee-next/commit/${deployedSHA}" target="_blank" rel="noopener"><code>${deployedSHA}</code></a>`);
 	isVercel && (str += ` on Vercel`);
 	return str;
-}
+});
 </script>
 
 <template>
@@ -86,7 +86,7 @@ const genBuildInfoStr = () => {
 	<router-view />
 	<footer>
 		<span class="footer-left">
-			<p v-if="isPreview" v-html="genBuildInfoStr()" />
+			<p v-if="isPreview" v-html="buildInfoStr" />
 			<p v-if="isPreview">This is a preview version, doesn't means final quality.</p>
 		</span>
 		<span class="footer-right">
